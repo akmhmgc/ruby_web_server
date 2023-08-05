41f25a3c51b7a70b8d1d732b8bbe9e9930e40843
akmhmgc <akimuhamaguchi@icloud.com>
2023-08-05 13:24:31 +0900

    Add a loop to the server to handle multiple requests

--- smallest_webserver.rb
+++ smallest_webserver.rb
@@ -3,14 +3,16 @@ require 'socket'
 port = 3000
 server = TCPServer.open(port)
 
-# wait for a connection
-socket = server.accept
-
 loop do
-  line = socket.readline.chomp
-  puts "Read line: #{line}"
+  # wait for a connection
+  socket = server.accept
 
-  if line.bytesize == 0
+  loop do
+    line = socket.readline.chomp
+    puts "Read line: #{line}"
+
+    next unless line.bytesize == 0
+
     socket.write "HTTP/1.1 200 OK\r\n"
     socket.write "\r\n"
     socket.write "Hello World!\r\n"
